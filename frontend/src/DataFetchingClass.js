1de5a2a2f35d39159d9340eab465997bbee61992
Omar Sayed <orhman@hotmail.com>
2022-05-06 20:00:34 +0200

    Add user input (name, zipcode, cuisine)

--- frontend/src/DataFetchingClass.js
+++ frontend/src/DataFetchingClass.js
@@ -4,4 +4,12 @@ export default class DataFetchingClass {
    static getAll(page = 0) {
         return configedAxios.get(`?page=${page}`);
     }
+    static getByQuery(queryName, queryValue) {
+        console.log("🚀 ~ file: DataFetchingClass.js ~ line 8 ~ DataFetchingClass ~ getByQuery ~ queryName, queryValue", queryName, queryValue)
+        return configedAxios.get(`?${queryName}=${queryValue}`);
+    }
+    static getAllCuisines() { 
+        return configedAxios.get(`/cuisines`);
+    }
+
 }
--- frontend/src/components/restaurant/RestaurantList.jsx
+++ frontend/src/components/restaurant/RestaurantList.jsx
@@ -1,5 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Container,
+  Dropdown,
+  DropdownButton,
+  Form,
+  FormControl,
+  InputGroup,
+  Row,
+} from "react-bootstrap";
 import DataFetchingClass from "../../DataFetchingClass";
 import CustomSpinner from "../CustomSpinner";
 import PaginationComponent from "../PaginationComponent";
@@ -7,24 +17,82 @@ import RestaurantCard from "./RestaurantCard";
 
 export default function RestaurantList() {
   const [restaurantList, setRestaurantsList] = useState([]);
+  const [cuisinesList, setCuisinesList] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalRestaurantCount, setTotalRestaurantCount] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
+  const [selectedFilter, setSelectedFilter] = useState("Filter");
+  const [userQuery, setUserQuery] = useState("");
+  const [isInvalidFilter, setInvalidFilter] = useState(false);
+  const [isInvalidCuisine, setIsInvalidCuisine] = useState(false);
+
+  const [selectedCuisine, setSelectedCuisine] = useState("Cuisines");
+  const handleFilterChange = (e) => {
+    setSelectedFilter(e);
+    setInvalidFilter(false);
+  };
+  const handleCuisineChange = (e) => {
+    setSelectedCuisine(e);
+    setIsInvalidCuisine(false);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const isInvalidFilter =
+      selectedFilter === "Filter" || selectedFilter === "Please Select";
+    const isInvalidCuisine =
+      (selectedCuisine === "Cuisines" || selectedCuisine === "Please Select") &&
+      selectedFilter === "cuisine";
+    if (isInvalidFilter) {
+      setInvalidFilter(true);
+    }
+    if (isInvalidCuisine) {
+      setIsInvalidCuisine(true);
+    } else if (!isInvalidCuisine && !isInvalidFilter) {
+      let response;
+      try {
+        setIsLoading(true);
+        if (selectedFilter === "cuisine") {
+          response = await DataFetchingClass.getByQuery(
+            selectedFilter,
+            selectedCuisine
+          );
+        } else {
+          response = await DataFetchingClass.getByQuery(
+            selectedFilter,
+            userQuery
+          );
+        }
+        setRestaurantsList(response.data.restaurantsList);
+        setTotalRestaurantCount(response.data.restaurantsList.length);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(`Error in fetching by query, ${error}`);
+      }
+    }
+  };
   useEffect(() => {
     const fetchAllRestaurants = async () => {
       try {
         setIsLoading(true);
-        const response = await await DataFetchingClass.getAll(currentPage);
-        console.log(
-          "🚀 ~ file: RestaurantList.jsx ~ line 8 ~ fetchAllRestaurants ~ response",
-          response
-        );
+        const response = await DataFetchingClass.getAll(currentPage);
+        const cuisines = (await DataFetchingClass.getAllCuisines()).data
+          .response;
+        // console.log(
+        // "🚀 ~ file: RestaurantList.jsx ~ line 71 ~ fetchAllRestaurants ~ cuisines",
+        // cuisines
+        // );
         const { restaurantsList } = response.data;
+        // // console.log("🚀 ~ file: RestaurantList.jsx ~ line 72 ~ fetchAllRestaurants ~ restaurantsList", restaurantsList)
+
         setRestaurantsList(restaurantsList);
+        setCuisinesList(cuisines);
         setTotalRestaurantCount(response.data.totalNumRestaurants);
         setIsLoading(false);
       } catch (error) {
-        console.error(`Error in fetching restaurant data, ${error}`);
+        console.error(
+          `Error in fetching restaurant data or cuisines, ${error}`
+        );
       }
     };
     fetchAllRestaurants();
@@ -43,6 +111,58 @@ export default function RestaurantList() {
   }
   return (
     <Container>
+      <Row className="mt-5 justify-content-center">
+        <Col xs={8} md={6} className="d-flex justify-content-center">
+          <Form onSubmit={handleSubmit}>
+            <InputGroup className="mb-3">
+              {selectedFilter !== "cuisine" && (
+                <FormControl
+                  required
+                  onChange={(e) => setUserQuery(e.target.value)}
+                  value={userQuery}
+                  aria-label="Text input with dropdown button"
+                />
+              )}
+
+              <DropdownButton
+                variant="outline-dark"
+                title={
+                  isInvalidFilter
+                    ? "Please Select"
+                    : `${selectedFilter[0].toUpperCase()}${selectedFilter.slice(
+                        1
+                      )}`
+                }
+                id="input-group-dropdown-1"
+                align="end"
+                onSelect={handleFilterChange}
+              >
+                <Dropdown.Item eventKey="name">Name</Dropdown.Item>
+                <Dropdown.Item eventKey="zipcode">Zipcode</Dropdown.Item>
+                <Dropdown.Item eventKey="cuisine">Cuisine</Dropdown.Item>
+              </DropdownButton>
+              {selectedFilter === "cuisine" && (
+                <DropdownButton
+                  variant="outline-dark"
+                  title={isInvalidCuisine ? "Please Select" : selectedCuisine}
+                  id="input-group-dropdown-2"
+                  align="end"
+                  onSelect={handleCuisineChange}
+                >
+                  {cuisinesList.map((cuisine, index) => {
+                    return (
+                      <Dropdown.Item key={index} eventKey={cuisine}>
+                        {cuisine.slice(0, 20)}
+                      </Dropdown.Item>
+                    );
+                  })}
+                </DropdownButton>
+              )}
+              <Button type="submit" variant="outline-dark" />
+            </InputGroup>
+          </Form>
+        </Col>
+      </Row>
       <Row className="my-5 gy-5  justify-content-center">{restaurants}</Row>
       <Row className="mb-2 ">
         <PaginationComponent
